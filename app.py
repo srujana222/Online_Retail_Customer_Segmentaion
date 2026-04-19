ca067d9ea6b4799dce5e52fa43d506a366a99aca
srujana222 <badiginchalasrujana222@gmail.com>
2026-04-19 16:06:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,35 @@
+import joblib
+import numpy as np
 from fastapi import FastAPI
 from pydantic import BaseModel
-import pickle,joblib
-import numpy as np
 
 app = FastAPI()
 
-model = joblib.load("model.pkl")
-import joblib
-scaler = joblib.load("scaler.pkl")
-label_encoder = joblib.load("label_encoder.pkl")
 
-class CustomerData(BaseModel):
-    age: float
+model = joblib.load("model.pkl")
+scaler = joblib.load("scaler.pkl")
+
+
+class Customer(BaseModel):
+    age: int
     annual_income: float
-    months_active: float
+    months_active: int
     avg_monthly_spend: float
-    purchase_frequency: float
+    purchase_frequency: int
     avg_order_value: float
     discount_usage_rate: float
     return_rate: float
     browsing_time_minutes: float
-    support_interactions: float
-    payment_method:str
-    region:str      
+    support_interactions: int
+    payment_method: str
+    region: str
 
-@app.get("/")
-def home():
-    return {"message": "Customer Classification API is running"}
 
 @app.post("/predict")
-def predict(data: CustomerData):
-     payment_map = {
+def predict(data: Customer):
+
+    # 🔹 Encoding maps
+    payment_map = {
         "Credit Card": 0,
         "Debit Card": 1,
         "UPI": 2,
@@ -43,6 +42,8 @@ def predict(data: CustomerData):
         "East": 2,
         "West": 3
     }
+
+    
     payment = payment_map.get(data.payment_method, 0)
     region = region_map.get(data.region, 0)
 
@@ -58,14 +59,12 @@ def predict(data: CustomerData):
         data.return_rate,
         data.browsing_time_minutes,
         data.support_interactions,
-        data.payment_method,
-        data.region       
+        payment,
+        region
     ]])
 
-    input_scaled = scaler.transform(input_data)
-    prediction = model.predict(input_scaled)
-    result = label_encoder.inverse_transform(prediction)
+    
+    scaled = scaler.transform(input_data)
+    prediction = model.predict(scaled)
 
-    return {
-        "prediction": result[0]
-    }
+    return {"prediction": int(prediction[0])}
